4ed3c1ea90a20e7fcf55ecd40b859f1ab53c6f9e
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:35:27 +0100

    java file

---
+++ ca269/TenToOne.java
@@ -0,0 +1,14 @@
+public class TenToOne
+{
+    public static void main(String [] args)
+    {
+        int i = 10;
+        while(i > 0)
+        {
+            System.out.print(i + " ");
+            i--;
+        }
+       
+        System.out.println();
+    }
+}
