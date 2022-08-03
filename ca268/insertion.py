c4ff2fd2482aa512daf614fa78baad8661f36ee2
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 16:57:46 +0100

    python file

---
+++ ca268/insertion.py
@@ -0,0 +1,13 @@
+#
+#   Return an empty list to get your list of numbers.
+#
+#   Then return a list of lists corresponding to the passes of an insertion sort on the numbers.
+#
+def solution():
+    return [
+        [2, 4],
+        [2, 4, 9],
+        [2, 3, 4, 9],
+        [1, 2, 3, 4, 9],
+        [1, 2, 3, 4, 8, 9]
+    ]
