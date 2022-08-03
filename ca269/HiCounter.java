d62617172616ed193c5f246c68af753d80a57af2
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:44:00 +0100

    java file

---
+++ ca269/HiCounter.java
@@ -0,0 +1,35 @@
+/*
+    This code is supplied and may be used to solve this problem.
+    
+    Note that the output matching is exact (down to spaces and newlines)
+    It will be very difficult to get this correct if you do not use the supplied 
+    System.out.print statements
+*/
+import java.util.Scanner;
+
+public class HiCounter
+{
+    public static void main(String [] args)
+    {
+        // Create a scanner object
+        Scanner in = new Scanner(System.in);
+        
+        System.out.print("Enter a phrase: ");
+        // Read in the phrase (actually the whole line)
+        String phrase = in.nextLine();
+
+        // count how many times "hi" occurs.
+        // YOUR CODE HERE
+        int length = phrase.length();
+        int count = 0;
+        for(int i = 0; i < length - 1; i++)
+        {
+            String sub_string = phrase.substring(i, i + 2);
+            //System.out.println(sub_string); 
+            if(sub_string.equals("hi")){
+                count = count + 1;
+            }
+        }
+        System.out.println(count);
+    }
+}
