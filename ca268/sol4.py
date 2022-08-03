3d919b9410370203d7f6be5156aa8622bfeb6906
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:10:43 +0100

    python file

---
+++ ca268/sol4.py
@@ -0,0 +1,8 @@
+#
+#   Return an empty list to get your list of numbers.
+#
+#   Then return a list of 10 lists representing the buckets
+#
+def sol():
+    return [80, 212, 33, 103, 254, 194, 204, 285, 227, 208, 89]
+    # [227, 254, 33, 194, 208, 212, 285, 103, 204, 80, 89]
