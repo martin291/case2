7b73227311bcc54ea78369eda35dd01ad751ef05
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:35:04 +0100

    java file

---
+++ ca269/FiveToTwenty.java
@@ -0,0 +1,14 @@
+public class FiveToTwenty
+{
+    public static void main(String [] args)
+    {
+        int i = 5;
+        while(i < 21)
+        {
+            System.out.print(i + " ");
+            i++;
+        }
+       
+    System.out.println();
+    }
+}
