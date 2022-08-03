b925ce822ac34a02b460c60130a0c3da4901a358
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:37:01 +0100

    java file

---
+++ ca269/OneToTen2.java
@@ -0,0 +1,14 @@
+public class OneToTen
+{
+    public static void main(String [] args)
+    {
+        // Use a for loop to print out the numbers.
+        
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.print((i + 1) + " ");
+        }
+        
+        System.out.println();
+    }
+}
