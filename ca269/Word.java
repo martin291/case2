e6079dfc6239717eb444b0c05f2a9cc50f4f0420
Martin Derwin <75258001+martin291@users.noreply.github.com>
2022-08-03 17:52:02 +0100

    java file

---
+++ ca269/Word.java
@@ -0,0 +1,15 @@
+// Create a Word class with one static method called isFirstLetter
+public class Word
+{
+    public static boolean isFirstLetter(String word, char letter)
+    {
+        if(word.charAt(0) == letter)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
